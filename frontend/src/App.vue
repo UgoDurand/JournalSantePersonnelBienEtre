cb91ee624cb052c0e98489b1b845ef19ed3b7896
Ugo <durand.ugo77@gmail.com>
2025-06-30 10:45:38 +0200

    Update contenu existant pour intégration de la consultation des données "sleep"

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,16 @@
+<!-- src/App.vue -->
 <template>
-  <HomePage/>
+  <!-- Sur mobile / zoom fort : colonne (navbar en haut), sur md+ : ligne (navbar à gauche) -->
+  <div class="flex flex-col md:flex-row min-h-screen">
+    <Navbar />
+    <!-- Le main doit pouvoir défiler indépendamment -->
+    <main class="flex-1 overflow-auto min-w-0">
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script>
-
-import HomePage from "@/pages/HomePage.vue";
-
-export default {
-  name: 'App',
-  components: {
-    HomePage
-  }
-}
+import Navbar from '@/components/Navbar.vue'
+export default { components: { Navbar } }
 </script>
