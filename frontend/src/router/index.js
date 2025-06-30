8c9a56bc600cc8ca1f24bb5cfa1fabd62231e17d
Ugo <durand.ugo77@gmail.com>
2025-06-30 15:04:23 +0200

    Add page activity + route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import SleepDetails from '../pages/SleepDetails.vue'
+import ActivityDetails from "@/pages/ActivityDetails.vue";
 
 const routes = [
     { path: '/',      name: 'Home',  component: HomePage },
-    { path: '/sleep', name: 'Sleep', component: SleepDetails }
+    { path: '/sleep', name: 'Sleep', component: SleepDetails },
+    { path: '/activity', name: 'Activity', component: ActivityDetails },
 ]
 
 const router = createRouter({
